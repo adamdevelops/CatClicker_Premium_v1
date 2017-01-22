9f4f5f0191eda93e1f9b3126f10edcd4c575a6f0
Adam <adamh93@verizon.net>
2017-01-22 12:38:13 -0500

    task: Create a cat clicker with even more cats added  chore: Add a README, automate the cat clicker displayed

---
+++ js/script.js
@@ -0,0 +1,88 @@
+var kittycats = [{
+  name: "Xuxa",
+  picture: "/img/xuxa.jpg"
+},
+{
+  name: "Chewie",
+  picture: "/img/chewie.jpg"
+},
+{
+  name: "Gato",
+  picture: "/img/gato.jpg"
+},
+{
+  name: "Mittens",
+  picture: "/img/mittens.jpg"
+},
+{
+  name: "Fluffy",
+  picture: "/img/fluffy.jpg"
+}];
+
+
+// //Cat 1
+// var addUp1 = function() {
+//   count1 += 1;
+//   document.getElementById("counter1").innerHTML = "This has been clicked " + count1 + " times.";
+//
+// };
+//
+// // Attach as a listener
+// document.getElementById("cat1").addEventListener("click", addUp1, false);
+//
+//
+// //Cat 2
+// var addUp2 = function() {
+//   count2 += 1;
+//   document.getElementById("counter2").innerHTML = "This has been clicked " + count2 + " times.";
+//
+// };
+//
+//
+// // Attach as a listener
+// document.getElementById("cat2").addEventListener("click", addUp2, false);
+
+// // clear the screen for testing
+// document.body.innerHTML = '';
+
+
+
+// // Let's loop over the numbers in our array
+// for (var i = 0; i < nums.length; i++) {
+//
+//     // This is the number we're on...
+//     var num = nums[i];
+//
+//     // We're creating a DOM element for the number
+//     var elem = document.createElement('div');
+//     elem.textContent = num;
+//
+//     // ... and when we click, alert the value of `num`
+//     elem.addEventListener('click', (function(numCopy) {
+//         return function() {
+//             alert(numCopy);
+//         };
+//     })(num));
+//
+//     document.body.appendChild(elem);
+// };
+
+
+for (var i = 0; i < kittycats.length; i++) {
+
+    // This is the number we're on...
+    var cat = kittycats[i];
+
+    // We're creating a DOM element for the number
+    var elem = document.createElement('li');
+    elem.textContent = cat.name;
+
+    // ... and when we click, alert the value of `num`
+    elem.addEventListener('click', (function(cat.name) {
+        return function() {
+            alert(cat.name);
+        };
+    })(cat));
+
+    document.body.appendChild(elem);
+};
